2be4be59a4c9eb6116b912d58db7339df294f4b4
CherylMerng <nskmerng@gmail.com>
2024-02-04 10:57:55 +0630

    Day1 - Controller

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
+
+// Practice Routing
+/* 
+Route::get('/articles', function() {
+    return "Article List";
+} );
+
+Route::get('/articles/detail/{id}', function($id) {
+    return  "Detail Page $id";
+} );
+*/
